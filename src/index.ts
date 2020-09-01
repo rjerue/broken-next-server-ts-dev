97486b9e23994b0e2061322822688639355d105c
Ryan Jerue <ryan.jerue@viasat.com>
2020-09-01 14:53:02 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req: any, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname === "/a") {
+      app.render(req, res, "/a", query);
+    } else if (pathname === "/b") {
+      app.render(req, res, "/b", query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3000, () => {
+    console.log("> Ready on http://localhost:3000");
+  });
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const Page = () => <div>Hello world</div>;
+
+export default Page;
